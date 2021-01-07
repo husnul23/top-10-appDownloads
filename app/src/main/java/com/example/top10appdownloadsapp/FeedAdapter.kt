6daa02d9a4bd90f6f0f28af5dcf7add2a1542bf9
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-01-07 11:22:15 +0700

    remove un used impot

--- app/src/main/java/com/example/top10appdownloadsapp/FeedAdapter.kt
+++ app/src/main/java/com/example/top10appdownloadsapp/FeedAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.top10appdownloadsapp
 
 import android.content.Context
-import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
